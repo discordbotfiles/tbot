4c5d249bd24d4ce0fda9a80abd39885264dd85a8
discordbotfiles <stupiddiscordbot@gmail.com>
2025-07-20 13:36:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ const commandsPath = path.join(__dirname, 'cmds');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
   const command = require(path.join(commandsPath, file));
+if (command.data && command.data.name) {
   client.commands.set(command.data.name, command);
+}
+
 }
 
 // Event Handlers
